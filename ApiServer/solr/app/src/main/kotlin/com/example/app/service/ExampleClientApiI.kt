c2f62ca0f0e65dcdf11f915a01b8803c40a711a1
natuty <595407835@qq.com>
2019-03-19 00:10:20 +0800

    commit

--- ApiServer/solr/app/src/main/kotlin/com/example/app/service/ExampleClientApiI.kt
+++
@@ -1,9 +0,0 @@
-package com.example.app.service
-
-import com.googlecode.jsonrpc4j.JsonRpcParam
-
-
-
-interface  ExampleClientApiI{
-    fun multiplier(@JsonRpcParam(value = "a") a: Int, @JsonRpcParam(value = "b") b: Int): Int
-}
